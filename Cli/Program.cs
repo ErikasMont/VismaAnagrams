a85fa74eb222ecf68885cbbf76e8d4a95f363961
erikasmont <erikas.montvydas105@gmail.com>
2022-06-06 18:47:30 +0300

    Main task done

--- Cli/Program.cs
+++ Cli/Program.cs
@@ -1,3 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using BusinessLogic.Repositories;
+using BusinessLogic.Services;
+using Cli;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Cli.Settings;
+using Contracts.Interfaces;
 
-Console.WriteLine("Hello, World!");
+string path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+
+IConfiguration config = new ConfigurationBuilder()
+    .SetBasePath(path)
+    .AddJsonFile("appsettings.json")
+    .AddEnvironmentVariables()
+    .Build();
+
+using IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((_, services) =>
+        services.AddScoped<IAnagramSolver, AnagramSolver>()
+            .AddScoped<IWordRepository, WordRepository>())
+    .Build();
+
+Settings settings = config.GetRequiredSection("Settings").Get<Settings>();
+
+var ui = new Cli.UI(host.Services.GetService<IAnagramSolver>(), settings.AnagramCount, settings.MinInputLength);
+ui.Run();
---
+++ Cli/UI.cs
@@ -0,0 +1,85 @@
+using BusinessLogic.Services;
+using Contracts.Interfaces;
+using Contracts.Models;
+
+namespace Cli;
+
+public class UI
+{
+    private IAnagramSolver _anagramSolver;
+    private int _anagramsCount;
+    private int _minLength;
+    public UI(IAnagramSolver anagramSolver, int anagramsCount, int minLength)
+    {
+        _anagramSolver = anagramSolver;
+        _anagramsCount = anagramsCount;
+        _minLength = minLength;
+    }
+
+    public void Run()
+    {
+        bool flag = true;
+        Console.WriteLine("Hello! Welcome to anagram solver app");
+        while (flag)
+        {
+            UserInput();
+            flag = ExitChoice();
+        }
+    }
+
+    private void UserInput()
+    {
+        Console.WriteLine("Please type in the word that you would like to get an anagram for: ");
+        string word = Console.ReadLine();
+        if (string.IsNullOrEmpty(word))
+        {
+            Console.WriteLine("No word given. Try again.");
+        }
+        else if(word.Length < _minLength)
+        {
+            Console.WriteLine("The word was too short. You need to enter a word that has at least {0} characters.", _minLength);
+        }
+        else
+        {
+            List<Anagram> anagrams = _anagramSolver.GetAnagrams(word, _anagramsCount);
+            if (anagrams == null || anagrams.Count == 0)
+            {
+                Console.WriteLine("No anagrams were found for the given word.");
+            }
+            else
+            {
+                PrintAnagrams(anagrams);
+            }
+        }
+    }
+
+    private bool ExitChoice()
+    {
+        Console.WriteLine("Would you like to exit? (y/n): ");
+        string choice = Console.ReadLine();
+        if (string.IsNullOrEmpty(choice) || (choice != "y" && choice != "n"))
+        {
+            Console.WriteLine("Wrong input given. Exiting.");
+            return false;
+        }
+        else if(choice == "y")
+        {
+            Console.WriteLine("Exiting.");
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    private void PrintAnagrams(List<Anagram> anagrams)
+    {
+        Console.Write("Anagrams: ");
+        foreach (var anagram in anagrams)
+        {
+            Console.Write("{0} ", anagram.Word);
+        }
+    }
+    
+}
