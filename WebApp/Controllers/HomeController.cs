99ff9fffb9de9761eef4af04a3ef9d79b52f401e
erikasmont <erikas.montvydas105@gmail.com>
2022-06-13 18:53:07 +0300

    Web app done and home controller tested.

---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Contracts.Interfaces;
+using Contracts.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using WebApp.Helpers;
+
+namespace WebApp.Controllers;
+public class HomeController : Controller
+{
+    private readonly IAnagramSolver _anagramSolver;
+    private readonly IWordRepository _wordRepository;
+    private readonly WordSettings _wordSettings;
+
+    public HomeController(IAnagramSolver anagramSolver, IWordRepository wordRepository, 
+        IOptions<WordSettings> wordSettings)
+    {
+        _anagramSolver = anagramSolver;
+        _wordRepository = wordRepository;
+        _wordSettings = wordSettings.Value;
+    }
+    
+    public IActionResult Index(string? word)
+    {
+        if (word == null)
+        {
+            return View("Index");
+        }
+        var anagrams = _anagramSolver.GetAnagrams(word, _wordSettings.AnagramCount);
+        return View("Index", anagrams);
+    }
+
+    public IActionResult AllWordsList(int? pageNumber)
+    {
+        var words = _wordRepository.ReadWords().Distinct().ToList();
+
+        return View("AllWordsList", PaginatedList<Word>.Create(words, pageNumber ?? 1, 100));
+    }
+}
